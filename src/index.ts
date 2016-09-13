82d04cea14f05ab3f9c31567ee7a74ddfe727755
Tiago Schenkel <tschenkel@goodgamestudios.com>
2016-09-13 14:36:23 +0200

    signalcommandmap: update copyright message on source headers

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+// ------------------------------------------------------------------------------
+//  Copyright (c) 2016 Goodgame Studios. All Rights Reserved.
+//
+//  NOTICE: You are permitted to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+// ------------------------------------------------------------------------------
+
 /**
  * SignalCommandMap Extension
  */
