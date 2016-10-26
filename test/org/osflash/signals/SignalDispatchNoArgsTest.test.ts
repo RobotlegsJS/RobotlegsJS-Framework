20f88d0fb1317b136ce3097de4dd93ed03cc6fab
boczujda <boczujda@goodgamestudios.com>
2016-10-26 14:58:51 +0200

    signals: Add SignalDispatchNoArgsTest

---
+++ test/org/osflash/signals/SignalDispatchNoArgsTest.test.ts
@@ -0,0 +1,40 @@
+import {assert} from "chai";
+
+import {AsyncUtil} from "../../../util/AsyncUtil";
+import {ISignal} from "../../../../src/org/osflash/signals/ISignal";
+import {Signal} from "../../../../src/org/osflash/signals/Signal";
+
+describe("SignalDispatchNoArgsTest", () => {
+
+    let async: AsyncUtil = new AsyncUtil();
+
+    let signal: ISignal;
+
+    beforeEach(() => {
+        signal = new Signal();
+    });
+
+    afterEach(() => {
+        signal.removeAll();
+        signal = null;
+    });
+
+    it("dispatch_no_args_should_call_listener_with_no_args", (done) => {
+        signal.add(async.add(onCompleted, 10, done));
+        signal.dispatch();
+    });
+
+    function onCompleted(): void {
+        assert.equal(0, arguments.length);
+    }
+
+    it("addOnce_in_handler_and_dispatch_should_call_new_listener", (done) => {
+        signal.addOnce(async.add(addOnceInHandler, 10));
+        signal.dispatch(done);
+    });
+
+    function addOnceInHandler(done): void {
+        signal.addOnce(async.add(new Function, 10, done));
+        signal.dispatch();
+    }
+});
