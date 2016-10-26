0753364ad7c1d27c8b146558063d77ed9cd86250
boczujda <boczujda@goodgamestudios.com>
2016-10-26 17:45:34 +0200

    signals: Add RedispatchedEventTest

---
+++ test/org/osflash/signals/RedispatchedEventTest.test.ts
@@ -0,0 +1,40 @@
+import {assert} from "chai";
+
+import {AsyncUtil} from "../../../util/AsyncUtil";
+import {DeluxeSignal} from "../../../../src/org/osflash/signals/DeluxeSignal";
+import {GenericEvent} from "../../../../src/org/osflash/signals/events/GenericEvent";
+
+describe("RedispatchedEventTest", () => {
+
+    let async: AsyncUtil = new AsyncUtil();
+
+    let completed: DeluxeSignal;
+    let originalEvent: GenericEvent;
+
+    beforeEach(() => {
+        completed = new DeluxeSignal(this);
+    });
+
+    afterEach(() => {
+        completed.removeAll();
+        completed = null;
+    });
+
+    it("dispatch_event_already_dispatched_should_clone_it", (done) => {
+        completed.add(async.add(redispatchEvent, 10));
+        originalEvent = new GenericEvent();
+        completed.dispatch(originalEvent, done);
+    });
+
+    function redispatchEvent(e: GenericEvent, done): void {
+        (e.signal as DeluxeSignal).removeAll();
+        assert.equal(originalEvent, e);
+        completed.add(async.add(check_redispatched_event_is_not_original, 10, done));
+
+        completed.dispatch(originalEvent);
+    }
+
+    function check_redispatched_event_is_not_original(e: GenericEvent): void {
+        assert.notEqual(originalEvent, e);
+    }
+});
