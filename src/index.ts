1cf410cba90b81ba8d20024acd772189fbae413f
Tiago Schenkel <tschenkel@goodgamestudios.com>
2016-10-27 10:25:32 +0200

    signals: Expose all classes on index.ts.

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+export { DeluxeSignal } from "./org/osflash/signals/DeluxeSignal";
+export { GenericEvent } from "./org/osflash/signals/events/GenericEvent";
+export { IBubbleEventHandler } from "./org/osflash/signals/events/IBubbleEventHandler";
+export { IEvent } from "./org/osflash/signals/events/IEvent";
+export { IOnceSignal } from "./org/osflash/signals/IOnceSignal";
+export { IPrioritySignal } from "./org/osflash/signals/IPrioritySignal";
+export { ISignal } from "./org/osflash/signals/ISignal";
+export { ISlot } from "./org/osflash/signals/ISlot";
+export { MonoSignal } from "./org/osflash/signals/MonoSignal";
+export { OnceSignal } from "./org/osflash/signals/OnceSignal";
+export { PrioritySignal } from "./org/osflash/signals/PrioritySignal";
+export { Promise } from "./org/osflash/signals/Promise";
+export { Signal } from "./org/osflash/signals/Signal";
+export { Slot } from "./org/osflash/signals/Slot";
+export { SlotList } from "./org/osflash/signals/SlotList";
