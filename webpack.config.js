ba38e1810058d43f4a30f192bbae4bad69954eed
Tiago Schenkel <tschenkel@goodgamestudios.com>
2016-11-10 15:57:17 +0100

    signals: update ident size rules on .editorconfig

--- webpack.config.js
+++ webpack.config.js
@@ -2,45 +2,45 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = function(options) {
-    return {
-        entry: {
-            main: path.join(__dirname, "src/index.ts")
-        },
+  return {
+    entry: {
+      main: path.join(__dirname, "src/index.ts")
+    },
 
-        output: {
-            path: path.join(__dirname, "dist"),
-            filename: "signals.min.js",
+    output: {
+      path: path.join(__dirname, "dist"),
+      filename: "signals.min.js",
 
-            libraryTarget: "var",
-            library: "SignalsJS"
-        },
+      libraryTarget: "var",
+      library: "SignalsJS"
+    },
 
-        devtool: 'inline-source-map',
+    devtool: 'inline-source-map',
 
-        module: {
-            rules: [
-                { test: /\.ts$/, loader: "ts-loader" },
-                {
-                    test: ((options.production) /* disable this loader for production builds */
-                        ? /^$/
-                        : /^(.(?!\.test))*\.ts$/),
-                    loader: "istanbul-instrumenter-loader",
-                    query: {
-                        embedSource: true
-                    },
-                    enforce: "post"
-                }
-            ]
-        },
-
-        plugins: (
-            (options.production)
-                ? [ new webpack.optimize.UglifyJsPlugin({ sourceMap: false }) ]
-                : [ new webpack.SourceMapDevToolPlugin({ test: /\.ts$/i }) ]
-        ),
-
-        resolve: {
-            extensions: ['.ts', '.js', '.json']
+    module: {
+      rules: [
+        { test: /\.ts$/, loader: "ts-loader" },
+        {
+          test: ((options.production) /* disable this loader for production builds */
+            ? /^$/
+            : /^(.(?!\.test))*\.ts$/),
+          loader: "istanbul-instrumenter-loader",
+          query: {
+            embedSource: true
+          },
+          enforce: "post"
         }
+      ]
+    },
+
+    plugins: (
+      (options.production)
+        ? [ new webpack.optimize.UglifyJsPlugin({ sourceMap: false }) ]
+        : [ new webpack.SourceMapDevToolPlugin({ test: /\.ts$/i }) ]
+    ),
+
+    resolve: {
+      extensions: ['.ts', '.js', '.json']
     }
+  }
 };
