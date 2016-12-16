898648d0b731927fb3e7fb87a8a613af93831256
CuongDD <cuongdd@sandinh.net>
2016-12-16 13:49:37 +0700

    pixi-signalmediator: add symbol to ISignalMap

--- src/robotlegs/bender/extensions/signalMediator/api/ISignalMap.ts
+++ src/robotlegs/bender/extensions/signalMediator/api/ISignalMap.ts
@@ -3,6 +3,7 @@ import {ISignal} from "signals.js";
 /**
  * The Signal Map allows you to bind Signals to functions
  */
+export let ISignalMap = Symbol("ISignalMap");
 export interface ISignalMap {
     addToSignal(signal: ISignal, handler: Function): void;
 
