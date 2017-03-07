bd185e7e4703ea06edbfc6a789cdfdcf43dc53e1
Tiago Schenkel <tschenkel@goodgamestudios.com>
2017-03-07 11:53:03 +0100

    signals: add test case to cover setter of bubbles property on GenericEvent class

--- test/org/osflash/signals/GenericEventTest.test.ts
+++ test/org/osflash/signals/GenericEventTest.test.ts
@@ -28,6 +28,12 @@ describe("GenericEventTest", () => {
         assert.isTrue(bubblingEvent.bubbles);
     });
 
+    it("bubbles_roundtrips_through_setter()", () => {
+        let bubblingEvent: GenericEvent = new GenericEvent();
+        bubblingEvent.bubbles = true;
+        assert.isTrue(bubblingEvent.bubbles);
+    });
+
     it("clone_should_be_instance_of_original_event_class()", () => {
         let theClone: IEvent = instance.clone();
         assert.isTrue(theClone instanceof GenericEvent);
