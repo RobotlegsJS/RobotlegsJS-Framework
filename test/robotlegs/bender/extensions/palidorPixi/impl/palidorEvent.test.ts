8e99b009b1e90d651e25cbc131a7b31e7020bacb
RonaldoSetzer <Ronaldo.Setzer@gmail.com>
2017-09-02 09:23:43 -0300

    pixi-palidor: add PalidorEvent

---
+++ test/robotlegs/bender/extensions/palidorPixi/impl/palidorEvent.test.ts
@@ -0,0 +1,21 @@
+import "./../../../../../entry";
+
+import { PalidorEvent } from "../../../../../../src/robotlegs/bender/extensions/palidorPixi/events/PalidorEvent";
+
+import { assert } from "chai";
+import { Event } from "@robotlegsjs/core/";
+
+describe("PalidorEvent", () => {
+    describe("constants", () => {
+        it("should exist events to remove Views and FloatingViews", () => {
+            assert.exists(PalidorEvent.REMOVE_CURRENT_VIEW);
+            assert.exists(PalidorEvent.REMOVE_ALL_FLOATING_VIEWS);
+            assert.exists(PalidorEvent.REMOVE_LAST_FLOATING_VIEW_ADDED);
+        });
+    });
+
+    it("should extend from Event (@robotlegsjs/core)", () => {
+        let event: Event = new PalidorEvent("event");
+        assert.instanceOf(event, Event);
+    });
+});
