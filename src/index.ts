312d53bdf3b63d0d647b38f4cb839e5e697066d1
Tiago Schenkel <tiago.schenkel@gmail.com>
2017-09-03 14:53:22 +0200

    pixi-palidor: expose classes that can be imported from extension package

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,13 @@
 /**
  * PalidorPixiExtension Extension
  */
+export { IContainerController } from "./robotlegs/bender/extensions/palidorPixi/api/IContainerController";
+export { IFlowManager } from "./robotlegs/bender/extensions/palidorPixi/api/IFlowManager";
+export { IFlowViewMapping } from "./robotlegs/bender/extensions/palidorPixi/api/IFlowViewMapping";
+export { IPixiRootContainer } from "./robotlegs/bender/extensions/palidorPixi/api/IPixiRootContainer";
+export { PalidorEvent } from "./robotlegs/bender/extensions/palidorPixi/events/PalidorEvent";
+export { FlowManager } from "./robotlegs/bender/extensions/palidorPixi/impl/FlowManager";
+export { FlowViewMapping } from "./robotlegs/bender/extensions/palidorPixi/impl/FlowViewMapping";
+export { PixiContainerContoller } from "./robotlegs/bender/extensions/palidorPixi/impl/PixiContainerContoller";
+export { PixiRootContainer } from "./robotlegs/bender/extensions/palidorPixi/impl/PixiRootContainer";
 export { PalidorPixiExtension } from "./robotlegs/bender/extensions/palidorPixi/PalidorPixiExtension";
