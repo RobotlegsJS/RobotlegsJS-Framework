f8023c58f9e52a3f1954a0c18dcb9bd9511656ca
Tiago Schenkel <tiago.schenkel@gmail.com>
2017-09-24 22:54:26 +0200

    pixi-signalmediator: remove wrong unit test

--- test/robotlegs/bender/extensions/signalMediator/signalMediatorExtension.test.ts
+++ test/robotlegs/bender/extensions/signalMediator/signalMediatorExtension.test.ts
@@ -12,25 +12,16 @@ import { Context } from "@robotlegsjs/core";
 import { SignalMediatorExtension } from "../../../../../src/robotlegs/bender/extensions/signalMediator/SignalMediatorExtension";
 import { ISignalMap } from "../../../../../src/robotlegs/bender/extensions/signalMediator/api/ISignalMap";
 import { SignalMap } from "../../../../../src/robotlegs/bender/extensions/signalMediator/impl/SignalMap";
-import { RelaySignal } from "./support/Signals";
-import { Data } from "./support/Data";
 
 describe("SignalMediatorExtension", () => {
     let context: Context;
-    let handler: Function;
-    let DATA: number;
 
     beforeEach(() => {
         context = new Context();
-        DATA = 0;
-        handler = (data: Data) => {
-            DATA = data.value;
-        };
     });
 
     afterEach(() => {
         context = null;
-        DATA = 0;
     });
 
     it("signalMap is mapped into injector", () => {
@@ -41,19 +32,5 @@ describe("SignalMediatorExtension", () => {
         });
         context.initialize();
         assert.instanceOf(actual, SignalMap);
-    });
-
-    it("chained injections pass through injection targets", () => {
-        context.install(SignalMediatorExtension);
-        context.initialize();
-
-        let instance: ISignalMap = context.injector.get<ISignalMap>(ISignalMap);
-        let signal = context.injector.get<RelaySignal>(RelaySignal);
-
-        instance.addToSignal(signal, handler);
-
-        DATA = 0;
-        context.injector.get<RelaySignal>(RelaySignal).dispatch(new Data(3));
-        assert.equal(DATA, 3);
     });
 });
