d19906b782658c26ab66ff16d6bab9d467971b51
Tiago Schenkel <tiago.schenkel@gmail.com>
2017-09-24 22:07:56 +0200

    pixi-signalmediator: remove lib folder and add it to .gitignore

--- lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * SignalMediator Extension
- */
-export {ISignalMap} from "./robotlegs/bender/extensions/signalMediator/api/ISignalMap";
-export {SignalMap} from "./robotlegs/bender/extensions/signalMediator/impl/SignalMap";
-export {SignalMediator} from "./robotlegs/bender/extensions/signalMediator/impl/SignalMediator";
-export {SignalMediatorExtension} from "./robotlegs/bender/extensions/signalMediator/SignalMediatorExtension";
--- lib/robotlegs/bender/extensions/signalMediator/SignalMediatorExtension.d.ts
+++
@@ -1,6 +0,0 @@
-import {IContext, IExtension} from "robotlegs";
-export declare class SignalMediatorExtension implements IExtension {
-    private _uid;
-    extend(context: IContext): void;
-    toString(): string;
-}
