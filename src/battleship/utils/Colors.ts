63be624566a2d809a9fafa386dce26b2a79dc487
RonaldoSetzer <Ronaldo.Setzer@gmail.com>
2017-10-24 09:43:12 -0200

    add utils

---
+++ src/battleship/utils/Colors.ts
@@ -0,0 +1,17 @@
+export class Colors {
+    public static BACKGROUND_DARK = 0x204d63;
+
+    public static TITLE = 0xffffff;
+    public static TEXT = 0xb5d6e6;
+
+    public static BUTTON_ICON = 0x204d63;
+
+    public static TILE_BACKGROUND = 0x307291;
+    public static TILE_BACKGROUND_OPEN = 0xb7d7e7;
+    public static TILE_BACKGROUND_MINE = 0xff7777;
+    public static TILE_BACKGROUND_MINE_2 = 0xfcf4a7;
+    public static TILE_TEXT_1 = 0x000099;
+    public static TILE_TEXT_2 = 0x009900;
+    public static TILE_TEXT_3 = 0x990000;
+    public static TILE_TEXT_4 = 0x999900;
+}
