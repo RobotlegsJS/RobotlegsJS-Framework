f3fb5b7995fd2667297f98d4d7e05ca218865170
Tiago Schenkel <tiago.schenkel@gmail.com>
2017-11-17 23:58:14 +0100

    macrobot: add unit tests for SequenceMacro class

---
+++ test/robotlegs/bender/utilities/macrobot/impl/sequenceMacro.test.ts
@@ -0,0 +1,80 @@
+// ------------------------------------------------------------------------------
+//  2017 RobotlegsJS. All Rights Reserved.
+//
+//  NOTICE: You are permitted to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+// ------------------------------------------------------------------------------
+
+import "../../../../../entry";
+
+import { assert } from "chai";
+
+import {
+    IContext,
+    IInjector,
+    ICommandMapping,
+    IEventDispatcher,
+    IEventCommandMap,
+    Context,
+    Event,
+    EventDispatcher
+} from "@robotlegsjs/core";
+
+import { EventCommandMap } from "@robotlegsjs/core/lib/robotlegs/bender/extensions/eventCommandMap/impl/EventCommandMap";
+
+import { SequenceMacro } from "../../../../../../src/robotlegs/bender/utilities/macrobot/impl/SequenceMacro";
+
+import { TestSequenceCommand } from "../support/TestSequenceCommand";
+import { TestSequenceWithPayloadsCommand } from "../support/TestSequenceWithPayloadsCommand";
+
+describe("SequenceMacro", () => {
+    let context: IContext;
+    let injector: IInjector;
+    let mappings: ICommandMapping[];
+    let dispatcher: IEventDispatcher;
+    let eventCommandMap: IEventCommandMap;
+    let reported: any[];
+
+    function reportingFunction(item: any): void {
+        reported.push(item);
+    }
+
+    beforeEach(() => {
+        context = new Context();
+        injector = context.injector;
+        injector
+            .bind("Function")
+            .toFunction(reportingFunction)
+            .whenTargetNamed("reportingFunction");
+        dispatcher = new EventDispatcher();
+        eventCommandMap = new EventCommandMap(context, dispatcher);
+        context.initialize();
+        reported = [];
+    });
+
+    afterEach(() => {
+        if (context.initialized) {
+            context.destroy();
+        }
+
+        context = null;
+        injector = null;
+        dispatcher = null;
+        eventCommandMap = null;
+        reported = null;
+    });
+
+    it("commands_are_executed_in_sequence", () => {
+        eventCommandMap.map("trigger", Event).toCommand(TestSequenceCommand);
+        dispatcher.dispatchEvent(new Event("trigger"));
+        assert.deepEqual(reported, [1, 2, 3]);
+    });
+
+    it("payload_instances_are_mapped", () => {
+        eventCommandMap
+            .map("trigger", Event)
+            .toCommand(TestSequenceWithPayloadsCommand);
+        dispatcher.dispatchEvent(new Event("trigger"));
+        assert.deepEqual(reported, ["Hello", "World", "!"]);
+    });
+});
