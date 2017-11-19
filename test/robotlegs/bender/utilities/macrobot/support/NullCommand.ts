215471a8059e490172a418ef755623231470466b
Tiago Schenkel <tiago.schenkel@gmail.com>
2017-11-19 15:41:23 +0100

    macrobot: add unit tests for SubCommandMappingList class

---
+++ test/robotlegs/bender/utilities/macrobot/support/NullCommand.ts
@@ -0,0 +1,17 @@
+// ------------------------------------------------------------------------------
+//  Copyright (c) 2017 RobotlegsJS. All Rights Reserved.
+//
+//  NOTICE: You are permitted to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+// ------------------------------------------------------------------------------
+
+import { injectable } from "inversify";
+
+import { ICommand } from "@robotlegsjs/core";
+
+@injectable()
+export class NullCommand implements ICommand {
+    public execute(): void {
+        // do nothing
+    }
+}
