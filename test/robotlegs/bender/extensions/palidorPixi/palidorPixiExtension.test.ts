173cc98318ed17f261ac66180f3c70773f03cfaa
Tiago Schenkel <tiago.schenkel@gmail.com>
2017-11-20 18:50:53 +0100

    pixi-palidor: improve code coverage of PalidorPixiExtension class

--- test/robotlegs/bender/extensions/palidorPixi/palidorPixiExtension.test.ts
+++ test/robotlegs/bender/extensions/palidorPixi/palidorPixiExtension.test.ts
@@ -15,41 +15,85 @@ import {
 
 import { assert } from "chai";
 import { Container } from "pixi.js";
-import { Context } from "@robotlegsjs/core";
-import { ContextView, PixiBundle } from "@robotlegsjs/pixi";
+
+import {
+    Context,
+    InjectableLoggerExtension,
+    EventDispatcherExtension,
+    DirectCommandMapExtension,
+    EventCommandMapExtension,
+    LocalEventMapExtension,
+    MVCSBundle
+} from "@robotlegsjs/core";
+
+import {
+    ContextView,
+    ContextViewExtension,
+    PixiBundle
+} from "@robotlegsjs/pixi";
 
 describe("PalidorPixiExtension", () => {
     let context: Context;
 
-    beforeEach(() => {
-        context = new Context();
-        context.install(PixiBundle);
-        context.install(PalidorPixiExtension);
-        context.configure(new ContextView(new Container()));
-    });
-
     afterEach(() => {
+        if (context.initialized) {
+            context.destroy();
+        }
         context = null;
     });
 
-    it("PalidorPixiExtension is mapped into injector", () => {
+    it("PalidorPixiExtension is properly initialized", () => {
         let initialized: boolean = false;
-        context.whenInitializing(function(): void {
+
+        context = new Context();
+        context.install(MVCSBundle, PixiBundle);
+        context.install(PalidorPixiExtension);
+        context.configure(new ContextView(new Container()));
+        context.whenInitializing(() => {
             initialized = true;
         });
         context.initialize();
+
         assert.isTrue(initialized);
     });
 
     it("FlowManager is mapped into injector", () => {
-        context.whenInitializing(function(): void {
+        context = new Context();
+        context.install(MVCSBundle, PixiBundle);
+        context.install(PalidorPixiExtension);
+        context.configure(new ContextView(new Container()));
+        context.afterInitializing(() => {
             assert.isDefined(context.injector.get(IFlowManager));
         });
+        context.initialize();
     });
 
     it("PixiContainerController is mapped into injector", () => {
-        context.whenInitializing(function(): void {
+        context = new Context();
+        context.install(MVCSBundle, PixiBundle);
+        context.install(PalidorPixiExtension);
+        context.configure(new ContextView(new Container()));
+        context.afterInitializing(() => {
             assert.isDefined(context.injector.get(IContainerController));
         });
+        context.initialize();
+    });
+
+    it("PalidorPixiExtension is not installed when IContextView is not provided", () => {
+        context = new Context();
+        context.install(
+            InjectableLoggerExtension,
+            EventDispatcherExtension,
+            DirectCommandMapExtension,
+            EventCommandMapExtension,
+            LocalEventMapExtension
+        );
+        context.install(PixiBundle);
+        context.install(PalidorPixiExtension);
+        context.afterInitializing(() => {
+            assert.isFalse(context.injector.isBound(IContainerController));
+            assert.isFalse(context.injector.isBound(IFlowManager));
+        });
+        context.initialize();
     });
 });
