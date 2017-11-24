422f8469ae32475306069e931252bc9b1bd22cb9
RonaldoSetzer <Ronaldo.Setzer@gmail.com>
2017-11-23 22:34:28 -0200

    update PixiFactory to support getText

--- src/utils/PixiFactory.ts
+++ src/utils/PixiFactory.ts
@@ -1,17 +1,22 @@
-import { Graphics } from 'pixi.js';
+import { Graphics, Text } from "pixi.js";
 
-import { Colors } from './Colors';
-import { MagicValues } from './MagicValues';
+import { Colors } from "./Colors";
+import { MagicValues } from "./MagicValues";
 
 export class PixiFactory {
     /* BACKGROUNDS */
     public static getColorBackground(color = Colors.BACKGROUND_DARK): Graphics {
         return this.getColorBox(MagicValues.MAX_WIDTH, MagicValues.MAX_HEIGHT, color);
     }
-    public static getColorBox(width: number, heigth: number, color = 0x00000): Graphics {
+    public static getColorBox(width: number, heigth: number, color = 0x000000): Graphics {
         const background: Graphics = new Graphics();
         background.beginFill(color);
         background.drawRect(0, 0, width, heigth);
         return background;
+    }
+    /* TEXTS */
+    public static getText(label: string, color: number = 0xffffff): Text {
+        const text = new Text(label, { fill: [color] });
+        return text;
     }
 }
