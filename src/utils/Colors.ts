df2bebd21d90fe71f94f5946b4c40bdbe1ae3968
RonaldoSetzer <Ronaldo.Setzer@gmail.com>
2017-11-23 22:25:10 -0200

    create TextButton component

--- src/utils/Colors.ts
+++ src/utils/Colors.ts
@@ -1,6 +1,7 @@
 export class Colors {
     public static BACKGROUND_DARK = 0x204d63;
+    public static BACKGROUND_LIGHT = 0xb5d6e6;
 
-    public static TITLE = 0xffffff;
     public static TEXT = 0xb5d6e6;
+    public static TEXT_DARK = 0x204d63;
 }
