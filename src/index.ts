3d3b8961f079118ed0e8816d719f70850d77c2b0
RonaldoSetzer <Ronaldo.Setzer@gmail.com>
2017-12-11 13:55:08 -0200

    pixi-palidor: create a PalidorBundle to simplify the intallation

--- src/index.ts
+++ src/index.ts
@@ -4,31 +4,15 @@
 //  NOTICE: You are permitted to use, modify, and distribute this file
 //  in accordance with the terms of the license agreement accompanying it.
 // ------------------------------------------------------------------------------
-
 /**
  * PalidorPixiExtension Extension
  */
-export {
-    IContainerController
-} from "./robotlegs/bender/extensions/palidorPixi/api/IContainerController";
-export {
-    IFlowManager
-} from "./robotlegs/bender/extensions/palidorPixi/api/IFlowManager";
-export {
-    IFlowViewMapping
-} from "./robotlegs/bender/extensions/palidorPixi/api/IFlowViewMapping";
-export {
-    PalidorEvent
-} from "./robotlegs/bender/extensions/palidorPixi/events/PalidorEvent";
-export {
-    FlowManager
-} from "./robotlegs/bender/extensions/palidorPixi/impl/FlowManager";
-export {
-    FlowViewMapping
-} from "./robotlegs/bender/extensions/palidorPixi/impl/FlowViewMapping";
-export {
-    PixiContainerContoller
-} from "./robotlegs/bender/extensions/palidorPixi/impl/PixiContainerContoller";
-export {
-    PalidorPixiExtension
-} from "./robotlegs/bender/extensions/palidorPixi/PalidorPixiExtension";
+export { IContainerController } from "./robotlegs/bender/extensions/palidorPixi/api/IContainerController";
+export { IFlowManager } from "./robotlegs/bender/extensions/palidorPixi/api/IFlowManager";
+export { IFlowViewMapping } from "./robotlegs/bender/extensions/palidorPixi/api/IFlowViewMapping";
+export { PalidorEvent } from "./robotlegs/bender/extensions/palidorPixi/events/PalidorEvent";
+export { FlowManager } from "./robotlegs/bender/extensions/palidorPixi/impl/FlowManager";
+export { FlowViewMapping } from "./robotlegs/bender/extensions/palidorPixi/impl/FlowViewMapping";
+export { PixiContainerContoller } from "./robotlegs/bender/extensions/palidorPixi/impl/PixiContainerContoller";
+export { PalidorPixiExtension } from "./robotlegs/bender/extensions/palidorPixi/PalidorPixiExtension";
+export { PalidorBundle } from "./robotlegs/bender/bundles/palidor/PalidorBundle";
