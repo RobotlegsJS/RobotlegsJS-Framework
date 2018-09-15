8ce6ce0fecd9ebabd6786f6c68e1d279fb65b89b
Tiago Schenkel <tiago.schenkel@gmail.com>
2018-09-15 17:04:49 +0200

    signals: solve name of each entry file generated by karma

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = env => {
   let mode = env.production ? "production" : "development";
   let tsconfig = !env.karma ? "tsconfig.json" : "tsconfig.test.json";
   let output = env.production ? "dist" : "dist-test";
-  let filename = env.production ? "signals.min.js" : "signals.js";
+  let filename = env.karma ? "[name].[hash].js" : (env.production ? "signals.min.js" : "signals.js");
 
   return {
     mode: mode,
