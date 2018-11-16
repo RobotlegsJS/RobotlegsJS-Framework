21ab19419d05766c842c306dab31def23d222542
Tiago Schenkel <tiago.schenkel@gmail.com>
2018-11-16 16:37:54 +0100

    macrobot: remove unused injections

--- test/robotlegs/bender/utilities/macrobot/support/CallbackParametersSequenceCommand.ts
+++ test/robotlegs/bender/utilities/macrobot/support/CallbackParametersSequenceCommand.ts
@@ -5,7 +5,7 @@
 //  in accordance with the terms of the license agreement accompanying it.
 // ------------------------------------------------------------------------------
 
-import { inject, injectable, named } from "@robotlegsjs/core";
+import { injectable } from "@robotlegsjs/core";
 
 import { SequenceMacro } from "../../../../../../src/robotlegs/bender/utilities/macrobot/impl/SequenceMacro";
 
@@ -13,31 +13,6 @@ import { CallbackParametersCommand } from "./CallbackParametersCommand";
 
 @injectable()
 export class CallbackParametersSequenceCommand extends SequenceMacro {
-    @inject(Boolean)
-    public booleanValue: boolean;
-
-    @inject(Number)
-    public numValue: number;
-
-    @inject(String)
-    public stringValue: string;
-
-    @inject(Symbol)
-    public symbolValue: symbol;
-
-    @inject(Object)
-    public objectValue: object;
-
-    @inject(Date)
-    public dateValue: Date;
-
-    @inject(Array)
-    public arrayValue: any[];
-
-    @inject("Function")
-    @named("reportingFunction")
-    public reportingFunction: Function;
-
     public prepare(): void {
         this.add(CallbackParametersCommand);
     }
