127722d79bca81f2174fdd451973739c263bdc0d
Tiago Schenkel <tiago.schenkel@gmail.com>
2018-11-23 22:04:07 +0100

    pixi-palidor: improve performance of karma

--- karma.conf.js
+++ karma.conf.js
@@ -9,24 +9,29 @@ const webpackConfig = require("./webpack.config.js")({ production: false, karma:
 delete webpackConfig.entry;
 
 module.exports = config => {
+  "use strict";
 
   var configuration = {
+    client: {
+      mocha: {
+        timeout : 5000
+      }
+    },
     basePath: "",
     frameworks: [
       "mocha",
-      "chai",
-      "sinon",
+      "sinon-chai",
       "es6-shim"
     ],
     files: [
       { pattern: "node_modules/reflect-metadata/Reflect.js", include: true },
       { pattern: "node_modules/bluebird/js/browser/bluebird.js", include: true },
-      { pattern: "./test/**/**/**.test.ts", include: true },
+      { pattern: "./test/index.ts", include: true },
       { pattern: '**/*.map', served: true, included: false, watched: true }
     ],
     preprocessors: {
-      "./**/**/**/**.ts": ["sourcemap"],
-      "./test/**/**/**.test.ts": ["webpack"]
+      "./test/index.ts": ["webpack"],
+      "./**/**/**/**.ts": ["sourcemap"]
     },
     webpack: webpackConfig,
     webpackMiddleware: {
@@ -39,11 +44,13 @@ module.exports = config => {
       "karma-remap-istanbul",
       "karma-mocha-reporter",
       "karma-mocha",
-      "karma-chai",
-      "karma-sinon",
+      "karma-sinon-chai",
       "karma-es6-shim",
       "karma-coverage-istanbul-reporter"
     ],
+    mime: {
+      "text/x-typescript": ["ts","tsx"]
+    },
     reporters: (
       config.singleRun ?
         ["dots", "mocha", "coverage-istanbul"] :
@@ -63,6 +70,7 @@ module.exports = config => {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
+    concurrency: Infinity,
     browsers: [],
     browserNoActivityTimeout: 50000
   };
