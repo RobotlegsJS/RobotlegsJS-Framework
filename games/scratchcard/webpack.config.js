72a3ad9f2d2c6b491688d2e7f628e73f7a5b2640
Tiago Schenkel <tiago.schenkel@gmail.com>
2021-06-12 18:00:00 +0100

    add script to generate production bundle

--- games/scratchcard/webpack.config.js
+++ games/scratchcard/webpack.config.js
@@ -1,10 +1,15 @@
 const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = (options) => {
+  if (!options) options = { production: false };
+
+  let mode = options.production ? "production" : "development";
+
   return {
-    mode: "development",
+    mode,
 
     entry: {
       main: path.resolve("src/index.ts")
@@ -15,13 +20,31 @@ module.exports = (options) => {
       filename: "bundle.js"
     },
 
-    devtool: "source-map",
+    devtool: options.production ? undefined : "source-map",
 
     module: {
       rules: [{ test: /\.ts$/, loader: "ts-loader" }]
     },
 
-    plugins: [new webpack.ProgressPlugin(), new HtmlWebpackPlugin()],
+    plugins: options.production ? [] : [new webpack.ProgressPlugin(), new HtmlWebpackPlugin()],
+
+    optimization: options.production
+      ? {
+          concatenateModules: true,
+          minimize: true,
+          minimizer: [
+            new TerserPlugin({
+              parallel: 4,
+              extractComments: false,
+              terserOptions: {
+                format: {
+                  comments: false
+                }
+              }
+            })
+          ]
+        }
+      : {},
 
     resolve: {
       extensions: [".ts", ".js", ".json"]
