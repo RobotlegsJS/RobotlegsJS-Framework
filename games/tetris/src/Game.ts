6167e21bbbd0e66ff2744cd3f1e70916818ef73e
Tiago Schenkel <tiago.schenkel@gmail.com>
2021-06-19 23:26:51 +0100

    ensure that reflect-metadata is loaded before game

---
+++ games/tetris/src/Game.ts
@@ -0,0 +1,34 @@
+/// <reference path="../node_modules/@robotlegsjs/pixi/definitions/pixi.d.ts" />
+
+import { Context } from "@robotlegsjs/core";
+import { ContextView } from "@robotlegsjs/pixi";
+import { PalidorBundle } from "@robotlegsjs/pixi-palidor";
+import { GameConfig } from "./configs/GameConfig";
+import { PalidorConfig } from "./configs/PalidorConfig";
+import { ViewsConfig } from "./configs/ViewsConfig";
+
+import PIXI = require("pixi.js");
+
+export class Game {
+    private _stage: PIXI.Container;
+    private _renderer: PIXI.Renderer;
+    private _context: Context;
+
+    public constructor() {
+        this._renderer = PIXI.autoDetectRenderer({ width: 340, height: 480 });
+        this._stage = new PIXI.Container();
+        this._context = new Context();
+        this._context
+            .install(PalidorBundle)
+            .configure(new ContextView(this._stage))
+            .configure(ViewsConfig, GameConfig, PalidorConfig)
+            .initialize();
+
+        document.body.appendChild(this._renderer.view);
+    }
+
+    public render = (): void => {
+        this._renderer.render(this._stage);
+        window.requestAnimationFrame(this.render);
+    };
+}
