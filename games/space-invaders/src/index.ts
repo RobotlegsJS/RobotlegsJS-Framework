825fb469d9c1320159d9229c702688fa7a3f3d2c
Tiago Schenkel <tiago.schenkel@gmail.com>
2021-06-19 23:26:42 +0100

    ensure that reflect-metadata is loaded before game

--- games/space-invaders/src/index.ts
+++ games/space-invaders/src/index.ts
@@ -1,39 +1,6 @@
-/// <reference path="../node_modules/@robotlegsjs/pixi/definitions/pixi.d.ts" />
-
-import { Context } from "@robotlegsjs/core";
-import { ContextView } from "@robotlegsjs/pixi";
-import { PalidorBundle } from "@robotlegsjs/pixi-palidor";
 import "reflect-metadata";
-import { GameConfig } from "./config/GameConfig";
-import { PalidorConfig } from "./config/PalidorConfig";
-import { ViewsConfig } from "./config/ViewsConfig";
+import { Game } from "./Game";
 
-import PIXI = require("pixi.js");
+const game: Game = new Game();
 
-class Main {
-    private _stage: PIXI.Container;
-    private _renderer: PIXI.Renderer;
-    private _context: Context;
-
-    public constructor() {
-        this._renderer = PIXI.autoDetectRenderer({ width: 340, height: 480 });
-        this._stage = new PIXI.Container();
-        this._context = new Context();
-        this._context
-            .install(PalidorBundle)
-            .configure(new ContextView(this._stage))
-            .configure(ViewsConfig, GameConfig, PalidorConfig)
-            .initialize();
-
-        document.body.appendChild(this._renderer.view);
-    }
-
-    public render = (): void => {
-        this._renderer.render(this._stage);
-        window.requestAnimationFrame(this.render);
-    };
-}
-
-const main: Main = new Main();
-
-main.render();
+game.render();
